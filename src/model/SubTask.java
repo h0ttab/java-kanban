7ce8dc785a16f4c481a730a566bfbfc71d451052
Petr <red19650226@yandex.ru>
2025-04-23 00:35:21 +0300

    Реализация класса SubTask
    Реализован класс для подзадач - SubTask. Каждая подзадача закреплена за определённым эпиком.

---
+++ src/model/SubTask.java
@@ -0,0 +1,14 @@
+package model;
+
+public class SubTask extends Task {
+    private final int epicId;
+
+    public SubTask(String title, String description, Status status, int epicId) {
+        super(title, description, status);
+        this.epicId = epicId;
+    }
+
+    public int getEpicId() {
+        return epicId;
+    }
+}
