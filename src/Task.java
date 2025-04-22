56b5c28d8e86d692eb1f6c543f12beee8a106bbe
Petr <red19650226@yandex.ru>
2025-04-22 23:53:17 +0300

    Реализован базовый класс Task

---
+++ src/Task.java
@@ -0,0 +1,65 @@
+import java.util.Objects;
+
+public class Task {
+    private int id;
+    private String title;
+    private String description;
+    private Status status;
+
+    public Task(String title, String description, Status status) {
+        this.title = title;
+        this.description = description;
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description.length='" + description.length() + '\'' +
+                ", status=" + status +
+                '}' + '\'';
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Task task = (Task) object;
+        return id == task.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
