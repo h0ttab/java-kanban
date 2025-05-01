fa9f0d7fea13623a6013e780c1003c702855c7b4
Petr <red19650226@yandex.ru>
2025-05-02 02:03:58 +0300

    Тесты для IdGenerator
    Добавлен тест для проверки последовательности и уникальности генерируемых ID.

---
+++ test/service/IdGeneratorTest.java
@@ -0,0 +1,21 @@
+package service;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IdGeneratorTest {
+    IdGenerator idGenerator = new IdGenerator();
+
+    @Test
+    @DisplayName("Генератор должен увеличивать внутренний счётчик ID на +1 после каждой генерации")
+    void shouldReturnUniqueId(){
+        int expectedFirstId = 1;
+        assertEquals(expectedFirstId, idGenerator.generateId());
+
+        int expected100thId = 100;
+        for (int i = 0; i < 98; i++) {
+            idGenerator.generateId();
+        }
+        assertEquals(expected100thId, idGenerator.generateId());
+    }
+}
