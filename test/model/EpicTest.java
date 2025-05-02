4bb189c2385016d139fb9fbf58972dcc97ea0f91
Petr <red19650226@yandex.ru>
2025-05-02 19:46:10 +0300

    Исправления в оформлении кода
    Приведено в соответствие с code-style, дублирующийся код создания тестовых эпиков вынесен в отдельный инициализирующий метод. ID по умолчанию задаются в инциализирующем методе, а в тех тестах, где требуются нестандартные ID - они переприсваиваются вручную.

--- test/model/EpicTest.java
+++ test/model/EpicTest.java
@@ -6,47 +6,44 @@ import service.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EpicTest {
+    private static Epic epicA;
+    private static Epic epicB;
+
+    @BeforeEach
+    void initEnv() {
+        epicA = new Epic("Тестовый эпик", "Описание тестового эпика");
+        epicB = new Epic("Тестовый эпик", "Описание тестового эпика");
+        epicA.setId(1);
+        epicB.setId(2);
+    }
 
     @Test
     @DisplayName("Два одинаковых эпика должны быть равны при сравнении через equals()")
-    void shouldConsiderTwoIdenticalTasksEqual(){
-
-        Epic epicA = new Epic("Тестовый эпик", "Описание тестового эпика");
+    void shouldConsiderTwoIdenticalTasksEqual() {
         epicA.setId(1);
-
-        Epic epicB = new Epic("Тестовый эпик", "Описание тестового эпика");
         epicB.setId(1);
-
         assertEquals(epicA, epicB);
     }
 
     @Test
     @DisplayName("Два одинаковых по содержимому эпика не должны быть равны при разных ID")
-    void shouldNotConsiderTwoIdenticalTasksWithDifferentIdEqual(){
-
-        Epic epicA = new Epic("Тестовый эпик", "Описание тестового эпика");
-        epicA.setId(1);
-
-        Epic epicB = new Epic("Тестовый эпик", "Описание тестового эпика");
-        epicB.setId(2);
-
+    void shouldNotConsiderTwoIdenticalTasksWithDifferentIdEqual() {
         assertNotEquals(epicA, epicB);
     }
 
     @Test
     @DisplayName("Эпик нельзя добавить в самого себя в виде подзадачи")
     void shouldNowAllowAddingEpicAsItsOwnSubtask() {
-        Epic epic = new Epic("Тестовый эпик", "Описание тестового эпика");
-        epic.setId(1);
-        assertNull(epic.addSubTask(1, Status.NEW));
+        assertNull(epicA.addSubTask(1, Status.NEW));
     }
 
     @Test
     @DisplayName("Вычисление статуса эпика происходит корректно")
-    void shouldUpdateEpicStatusCorrectly(){
+    void shouldUpdateEpicStatusCorrectly() {
         TaskManager taskManager = Managers.getDefault();
-        Epic epic = new Epic("Тестовый эпик", "Описание тестового эпика");
-        taskManager.createEpic(epic);
+
+        taskManager.createEpic(epicA);
+
         assertEquals(Status.NEW, taskManager.getEpicById(1).getStatus());
 
         taskManager.createSubTask(new SubTask("Подзадача",
