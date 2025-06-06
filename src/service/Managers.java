9ab14b7e468b56f08b375eb88d117752e9dddf30
Petr <red19650226@yandex.ru>
2025-06-06 12:08:26 +0300

    Добавить проверку наличия директории и файла сохранения

--- src/service/Managers.java
+++ src/service/Managers.java
@@ -15,8 +15,16 @@ public class Managers {
     public static TaskManager getFileBacked(String fileName) {
         IdGenerator idGenerator = new IdGenerator();
         HistoryManager historyManager = getDefaultHistory();
+        Path saveDirPath = Paths.get("resources");
+        Path saveFilePath = saveDirPath.resolve(fileName);
 
-        Path saveFilePath = Paths.get("resources", fileName);
+        if (Files.notExists(saveDirPath)) {
+            try {
+                Files.createDirectory(saveDirPath);
+            } catch (IOException e) {
+                System.out.println("Ошибка при создании директории \"resources\".");
+            }
+        }
 
         if (Files.notExists(saveFilePath)) {
             try {
