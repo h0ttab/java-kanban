aa0386951ae05e76d980159c429f45591d446e14
Petr <red19650226@yandex.ru>
2025-06-11 00:24:07 +0300

    Сделать класс Node внутренним классом InMemoryHistoryManager

--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -68,4 +68,48 @@ public class InMemoryHistoryManager implements HistoryManager {
         linkLast(newHistoryRecord);
     }
 
+    static final class Node<T> {
+        public Node<T> next;
+        public Node<T> prev;
+        public T value;
+
+        public Node(T value) {
+            this.value = value;
+        }
+
+        public void removeSelf() {
+           Node<T> prevNode;
+           Node<T> nextNode;
+
+            if (next != null) {
+                nextNode = next;
+                nextNode.prev = prev;
+            }
+
+            if (prev != null) {
+                prevNode = prev;
+                prevNode.next = next;
+            }
+        }
+
+        @Override
+        public boolean equals(Object object) {
+            if (this == object) return true;
+            if (object == null || getClass() != object.getClass()) return false;
+           Node<?> node = (Node<?>) object;
+            return Objects.equals(next, node.next) && Objects.equals(prev, node.prev) && Objects.equals(value, node.value);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(next, prev, value);
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "value=" + value +
+                    '}';
+        }
+    }
 }
--- test/service/InMemoryHistoryManagerTest.java
+++ test/service/InMemoryHistoryManagerTest.java
@@ -1,6 +1,7 @@
 package service;
 
 import model.*;
+import service.InMemoryHistoryManager.*;
 import org.junit.jupiter.api.*;
 
 import java.util.*;
@@ -73,5 +74,54 @@ public class InMemoryHistoryManagerTest {
         historyManager.addTask(updatedSubTask);
 
         assertEquals(expectedHistory, historyManager.getHistory());
+    }
+
+    @Nested
+    class NodeTest {
+        Node<String> testNodeA = new Node<>("Node A");
+        Node<String> testNodeB = new Node<>("Node B");
+        Node<String> testNodeC = new Node<>("Node C");
+
+        @BeforeEach
+        void linkTestNodes() {
+            testNodeA.next = testNodeB;
+            testNodeB.prev = testNodeA;
+            testNodeB.next = testNodeC;
+            testNodeC.prev = testNodeB;
+        }
+
+        @Test
+        @DisplayName("Узел A можно взаимно связать с узлом B")
+        void shouldLinkNodes() {
+            assertEquals(testNodeA.next, testNodeB);
+            assertEquals(testNodeB.prev, testNodeA);
+        }
+
+        @Test
+        @DisplayName("При удалении себя узел связывает соседние узлы")
+        void shouldLinkNeighborsUponRemoval() {
+            testNodeB.removeSelf();
+
+            // Удаление узла с соседями в обе стороны
+            assertEquals(testNodeA.next, testNodeC);
+            assertEquals(testNodeC.prev, testNodeA);
+
+            // Удаление узла с соседом только в одну сторону
+            testNodeC.removeSelf();
+            assertNull(testNodeA.next);
+        }
+
+        @Test
+        @DisplayName("Два идентичных узла равны друг другу")
+        void shouldBeEqualIfIdentical() {
+            Node<String> copyOfTestNodeB = new Node<>("Node B");
+            copyOfTestNodeB.prev = testNodeA;
+            copyOfTestNodeB.next = testNodeC;
+
+            // Намеренное использование assertTrue вместо assertEquals,
+            // чтобы проверить метод equals()
+            assertTrue(testNodeB.equals(copyOfTestNodeB));
+            assertNotEquals(testNodeB, testNodeC);
+        }
     }
 }
